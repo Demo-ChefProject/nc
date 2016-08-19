2bdf3d508c8471ae04ab0302ae67f5b6ca88f20f
Krithi Gnanasekaran <Krithi.Gnanasekaran@nc4.local>
2016-08-19 11:27:33 -0400

    Adding node_bootstrap in root and including cookbook inside apache

---
+++ apache/cookbooks/trusted_certs/node_bootstrap.ps1
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+#cd ~/home/jenkins/workspace/Chef-apache/
+cd C:\Jenkins\workspace\Chef-apache\
+
+# Creates a role for tomcat
+#sudo knife role from file /home/jenkins/workspace/Chef-apache/roles/apache.rb
+knife role from file C:\Jenkins\workspace\Chef-apache\roles\apache.rb
+
+
+# Upload cookbooks into Chef Server
+knife upload cookbooks apache
+
+# Bootstrap a node to its chef server
+# knife bootstrap windows winrm 54.175.57.21 --winrm-user Administrator --winrm-password 'd*G%tc9"&"HLK' --node-name Rigil_node_Windows -r 'role[apache]' -y
+
+#Passing credentials stored as a secure string
+$Pass = cat C:\securestring.txt | convertto-securestring
+$Pegacred = new-object -TypeName System.Management.Automation.PSCredential -argumentlist "Administrator",$pass
+#  Create a remote session to the chef node
+$Session = New-PSSession -ComputerName 54.175.57.21 -Credential $Pegacred
+
+$Script = {powershell.exe chef-client}
+
+$Job = Invoke-Command -Session $Session -Scriptblock $Script
+echo $Job
+#Script which runs the ruby script in the remote server
+
+
+# Exit and remove the current session
+Remove-PSSession -Session $Session
+
+
+# ssh into the chef node and execute the chef client to run its run list from chef server
+#ssh -i /home/ubuntu/.ssh/agiletrailblazers.pem ubuntu@54.175.232.159 "sudo chef-client"
