6fff96aee266e6c1487f27214e847cea2e9833a2
sananda1 <skumar@agiletrailblazers.com>
2016-08-20 14:10:36 -0400

    update bootstrap

--- node_bootstrap.ps1
+++ node_bootstrap.ps1
@@ -5,7 +5,7 @@ cd C:\Jenkins\workspace\Chef-apache
 
 # Creates a role for tomcat
 #sudo knife role from file /home/jenkins/workspace/Chef-apache/roles/apache.rb
-knife role from file C:\Jenkins\workspace\Chef-apache\roles\apache.rb
+knife role from file C:\Jenkins\workspace\Chef-apache\roles\apache.json
 
 
 # Upload cookbooks into Chef Server
