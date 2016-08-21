662ea418e05d7cdbc7cb773e3c0d6edc74e62e99
Krithi Gnanasekaran <kgnanasekaran@agiletrailblazers.com>
2016-08-21 01:46:26 -0400

    Update default.rb

--- cookbooks/apache/attributes/default.rb
+++ cookbooks/apache/attributes/default.rb
@@ -1,6 +1,6 @@
 #default['tomcat']['default']['port']  = "8081"
 #Variable definitions for apache cookbook
-default['nc4']['nexus']['url']="http://devmcnexus21.nc4.local:8081"
+default['nc4']['nexus']['url']=" http://54.175.158.124:8081/repository/Rigil/"
 default['nc4']['apache-httpd-32']['version']="/repository/maven-releases/org/apache/apache-httpd-32/2.2.32"
 default['nc4']['apache-httpd-32']['package']="/apache-httpd-32-2.2.32.zip"
 
