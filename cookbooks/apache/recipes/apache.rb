a1b526bf1f14b69bd1de5b0216e474d256b70e9f
Agile Trailblazers Jenkins Account <eric@agiletrailblazers.com>
2016-08-21 13:32:38 +0400

    Update apache.rb

--- cookbooks/apache/recipes/apache.rb
+++ cookbooks/apache/recipes/apache.rb
@@ -3,7 +3,7 @@ apache_download_location = "#{node['nc4']['nexus']['url']}#{node['nc4']['apache-
 # remote_file "Download Apache Module from nexus" do
   #source apache_download_location
 remote_file 'C:/NC4/MC3/apache-httpd-32-2.2.2.32.zip' do
-  source 'http://54.175.158.124:8081/repository/Rigil/apache-httpd-32-2.2.2.32.zip'
+  source 'http://54.175.158.124:8081/repository/Rigil/apache-httpd-32-2.2.32.zip'
   action :create
 #end
   notifies :run, 'execute[Unzip Apache package]', :immediately
