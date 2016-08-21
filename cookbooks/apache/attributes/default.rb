75693de9ab885f91f72108110899d817833cb78b
Agile Trailblazers Jenkins Account <eric@agiletrailblazers.com>
2016-08-21 14:13:47 +0400

    Update default.rb

--- cookbooks/apache/attributes/default.rb
+++ cookbooks/apache/attributes/default.rb
@@ -5,13 +5,13 @@ default['nc4']['apache-httpd-32']['version']="/repository/Rigil/apache-httpd-32-
 default['nc4']['apache-httpd-32']['package']="/apache-httpd-32-2.2.32.zip"
 
 #Apache install location variables
-default['nc4']['apache']['install_location'] = "D:\\NC4\\MC3"
-default['nc4']['apache']['workdir'] = "D:\\NC4\\MC3\\HTTPD"
-default['nc4']['apache']['logdir'] = "D:\\NC4\\MC3\\logs"
+default['nc4']['apache']['install_location'] = "C:\\NC4\\MC3"
+default['nc4']['apache']['workdir'] = "C:\\NC4\\MC3\\HTTPD"
+default['nc4']['apache']['logdir'] = "c:\\NC4\\MC3\\logs"
 
 #Newly added
-default['nc4']['apache']['bindir'] = "D:\NC4\HTTPD\bin"
+default['nc4']['apache']['bindir'] = "C:\NC4\HTTPD\bin"
 default['nc4']['httpd-vhost-conf']['url']="httpd-vhost.conf"
 default['nc4']['httpd-erb']['url']="httpd.erb"
-default['nc4']['mc3agiledev-conf']['url']  = "D:\\NC4\\MC3\\HTTPD\\conf\\extra\\MC3AgileDev.conf"
-default['nc4']['httpd-conf']['url'] = "D:\\NC4\\MC3\\HTTPD\\conf\\httpd.conf"
+default['nc4']['mc3agiledev-conf']['url']  = "C:\\NC4\\MC3\\HTTPD\\conf\\extra\\MC3AgileDev.conf"
+default['nc4']['httpd-conf']['url'] = "C:\\NC4\\MC3\\HTTPD\\conf\\httpd.conf"
