01a28dbc2e9b457a2acb00f4c1914288f622714d
Krithi Gnanasekaran <kgnanasekaran@agiletrailblazers.com>
2016-08-21 02:02:20 -0400

    Update metadata.rb

--- cookbooks/apache/metadata.rb
+++ cookbooks/apache/metadata.rb
@@ -5,5 +5,5 @@ license          'All rights reserved'
 description      'Installs/Configures apache httpd'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.1'
-issues_url       'https://github.com/Demo-ChefProject/nc.git'  if respond_to?(:issues_url)
-source_url       'https://github.com/Demo-ChefProject/nc.git' if respond_to?(:source_url)
+issues_url       'https://github.com/Demo-ChefProject/nc/cookbooks/apache/'  if respond_to?(:issues_url)
+source_url       'https://github.com/Demo-ChefProject/nc/cookbooks/apache/' if respond_to?(:source_url)
