a8623e8d366a508613206f5360aa4b0d611903c8
Krithi Gnanasekaran <kgnanasekaran@agiletrailblazers.com>
2016-08-22 11:39:28 -0400

    Update apache.rb

--- cookbooks/apache/recipes/apache.rb
+++ cookbooks/apache/recipes/apache.rb
@@ -46,7 +46,7 @@ end
 #end
 
 #template 'D:\NC4\MC3\HTTPD\conf\httpd.conf' do
-template 'C:\\NC4\\MC3\\HTTPD\\conf\\httpd.conf' do
+template 'C:\\NC4\\MC3\\HTTPD\\conf\\httpd.conf\\httpd.conf.erb' do
   source 'C:\\NC4\\MC3\\HTTPD\\conf\\httpd.conf\\httpd.conf.erb'
   #source '#node{["nc4"]["httpd-erb"]["url"]}'
   variables( :server_name => 'MC3AgileDev')
