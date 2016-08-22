f671ec30a8261a5c32a16e56caf56bd161600e5a
Krithi Gnanasekaran <kgnanasekaran@agiletrailblazers.com>
2016-08-22 11:52:55 -0400

    Update apache.rb

--- cookbooks/apache/recipes/apache.rb
+++ cookbooks/apache/recipes/apache.rb
@@ -40,7 +40,7 @@ end
 
 #file 'C:\NC4\MC3\HTTPD\conf\extra\MC3AgileDev.conf' do
 file '#node{["nc4"]["mc3agiledev-conf"]["url"]}'   do
-  path "C:\NC4\MC3\HTTPD\conf\extra\httpd-vhosts.conf"
+  path "C:\\NC4\\MC3\\HTTPD\\conf\\extra\\httpd-vhosts.conf"
 #  action :'#node{["nc4"]["httpd-vhost-conf"]["url"]}'
   action :create
 end
