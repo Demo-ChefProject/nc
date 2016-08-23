5cca75211db75dd0c8ab2afb2e35e01e2c8aab87
sananda1 <skumar@agiletrailblazers.com>
2016-08-23 15:36:56 -0400

    more change

--- cookbooks/apache/recipes/apache.rb
+++ cookbooks/apache/recipes/apache.rb
@@ -2,14 +2,15 @@
 #apache_download_location = "#{node['nc4']['nexus']['url']}#{node['nc4']['apache-httpd-32']['version']}#{node['nc4']['apache-httpd-32']['package']}"
 apache_download_location = "http://54.175.158.124:8081/repository/Rigil/apache-httpd-32-2.2.32.zip"
 apache_server_name = node['nc4']['server_name']
-apache_work_dir = node['nc4']['apache']['workdir']
-apache_httpd_conf = node['nc4']['apache-conf']['location']
+#apache_work_dir = node['nc4']['apache']['workdir']
+apache_work_dir = "#{node['nc4']['apache']['install_location']}/HTTPD"
+#apache_httpd_conf = node['nc4']['apache-conf']['location']
+apache_httpd_conf = "#{apache_work_dir}/conf"
 
 remote_file "Download Apache Module from nexus" do
   source apache_download_location
   action :create
   notifies :run, 'powershell_script[Unzip Apache package]', :immediately
-  #not_if {file.directory?('D:\NC4\MC3')}
 end
 
 powershell_script 'Unzip Apache package' do
@@ -17,7 +18,6 @@ powershell_script 'Unzip Apache package' do
   Remove-Item #{apache_work_dir} -recurse
   powershell.exe -nologo -noprofile -command "& { Add-Type -A 'System.IO.Compression.FileSystem'; [IO.Compression.ZipFile]::ExtractToDirectory('C:/NC4/MC3/apache-httpd-32-2.2.2.32.zip', 'C:/NC4/MC3'); }"
   EOH
-#  only_if "Dir.exist?(#node{['nc4']['apache']['workdir']})"
   notifies :run, 'powershell_script[Remove logs folder]', :immediately
 end
 
@@ -25,35 +25,20 @@ end
 powershell_script 'Remove logs folder' do
   guard_interpreter :powershell_script
   code <<-EOH
-    Remove-Item #{apache_work_dir}/logs -recurse
+    Remove-Item #{apache_work_dir}/logs/* -recurse
   EOH
   only_if do Dir.exist?("#{apache_work_dir}/logs") end
   notifies :run, 'powershell_script[Remove error folder]', :immediately
 end
 
-powershell_script 'Remove error folder' do
-  guard_interpreter :powershell_script
-  code <<-EOH
-    Remove-Item #{apache_work_dir}/error -recurse
-  EOH
-  only_if do Dir.exist?("#{apache_work_dir}/error") end
-end
-
-
-=begin
-powershell_script 'Remove Logs' do
-guard_interpreter :powershell_script
-code <<-EOH
-Remove-Item #{node['nc4']['apache']['workdir']}/error -recurse
-EOH
-only_if "Dir.exist?(#{node['nc4']['apache']['workdir']}/error')"
-code <<-EOH
-Remove-Item #{node['nc4']['apache']['workdir']}/logs -recurse
-EOH
-only_if "Dir.exist?(#{node['nc4']['apache']['workdir']}/logs)"
-end
-=end
-
+#Commented out per feedback from Satvinder
+#powershell_script 'Remove error folder' do
+#  guard_interpreter :powershell_script
+#  code <<-EOH
+#    Remove-Item #{apache_work_dir}/error -recurse
+#  EOH
+#  only_if do Dir.exist?("#{apache_work_dir}/error") end
+#end
 
 template "#{apache_httpd_conf}/httpd-vhost.conf" do
   source 'httpd-vhosts.conf.erb'
@@ -70,18 +55,28 @@ template "#{apache_httpd_conf}/httpd.conf" do
   action :create
 end
 
-powershell_script 'delete_if_exist' do
+#powershell_script 'delete_if_exist' do
+#  code <<-EOH
+#     $Service = Get-WmiObject -Class Win32_Service -Filter 'Name="Apache-HTTPD-2.2"'
+#     if ($Service) {
+#        $Service.Delete() 
+#     }
+#  EOH
+#  notifies :run, 'execute[Installing Service Apache]', :immediately
+#end
+
+powershell_script 'install Apache service if not exists' do
   code <<-EOH
-     $Service = Get-WmiObject -Class Win32_Service -Filter 'Name="Apache-HTTPD-2.2"'
-     if ($Service) {
-        $Service.Delete() 
+     $Service = Get-Service -Name Apache-HTTPD-2.2 -ErrorAction SilentlyContinue
+     if (! $Service) {
+          sc create Apache-HTTPD-2.2 binPath= \"#{apache_work_dir}/bin/httpd.exe\" start= auto DisplayName= \"Apache HTTPD 2.2\"
      }
   EOH
-  notifies :run, 'execute[Installing Service Apache]', :immediately
+#  notifies :run, 'execute[Installing Service Apache]', :immediately
 end
 
-execute 'Installing Service Apache' do
-  command "timeout /T 10"
-  command "sc create Apache-HTTPD-2.2 binPath= \"#{apache_work_dir}/bin/httpd.exe\" start= auto DisplayName= \"Apache HTTPD 2.2\""
-  action :nothing
-end
+#execute 'Installing Service Apache' do
+#  command "timeout /T 10"
+#  command "sc create Apache-HTTPD-2.2 binPath= \"#{apache_work_dir}/bin/httpd.exe\" start= auto DisplayName= \"Apache HTTPD 2.2\""
+#  action :nothing
+#end
