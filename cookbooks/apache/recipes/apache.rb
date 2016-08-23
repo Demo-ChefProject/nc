16b89dc063192e79c44eab363a25ecfefdaacb03
Krithi Gnanasekaran <kgnanasekaran@agiletrailblazers.com>
2016-08-23 11:49:25 -0400

    Update apache.rb

--- cookbooks/apache/recipes/apache.rb
+++ cookbooks/apache/recipes/apache.rb
@@ -21,6 +21,7 @@ powershell_script 'Unzip Apache package' do
   notifies :run, 'powershell_script[Remove Logs]', :immediately
 end
 
+=begin
 powershell_script 'Remove Logs' do
   guard_interpreter :powershell_script
   code <<-EOH
@@ -28,8 +29,9 @@ powershell_script 'Remove Logs' do
   Remove-Item #{node['nc4']['apache']['workdir']}/logs -recurse
   EOH
 end
+=end
 
-=begin
+
 powershell_script 'Remove Logs' do
 guard_interpreter :powershell_script
 code <<-EOH
@@ -41,7 +43,7 @@ Remove-Item #{node['nc4']['apache']['workdir']}/logs -recurse
 EOH
 only_if "Dir.exist?(#{node['nc4']['apache']['workdir']}/logs)"
 end
-=end
+
 
 template "#{apache_httpd_conf}/httpd-vhost.conf" do
   source 'httpd-vhosts.conf.erb'
