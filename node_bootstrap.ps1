b550d73c24188183bc521c98e7c39de5600df614
sananda1 <skumar@agiletrailblazers.com>
2016-08-25 13:27:26 -0400

    commit

--- node_bootstrap.ps1
+++ node_bootstrap.ps1
@@ -24,7 +24,9 @@ $Pegacred = new-object -TypeName System.Management.Automation.PSCredential -argu
 $Session = New-PSSession -ComputerName 54.175.57.21 -Credential $Pegacred
 
 #Script which runs the ruby script in the remote server
-echo "{ \"use_akamai\": %use_akamai% }" > ./test_jenkins.json
+echo "1st: %%use_akamai%% here"
+echo "2st: %use_akamai% here"
+echo "{ 'use_akamai': %%use_akamai%% }" > ./test_jenkins.json
 $Script = {powershell.exe chef-client -j ./test_jenkins.json}
 
 echo "*****"
