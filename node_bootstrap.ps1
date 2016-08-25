b5db8d062f3ac372abc8a844c7361755aeb3435f
sananda1 <skumar@agiletrailblazers.com>
2016-08-25 13:42:31 -0400

    chaging logic

--- node_bootstrap.ps1
+++ node_bootstrap.ps1
@@ -24,8 +24,9 @@ $Pegacred = new-object -TypeName System.Management.Automation.PSCredential -argu
 $Session = New-PSSession -ComputerName 54.175.57.21 -Credential $Pegacred
 
 #Script which runs the ruby script in the remote server
-pwd
-$Script = {powershell.exe chef-client -j C:\Jenkins\workspace\Chef-apache\test_jenkins.json}
+#pwd
+#$Script = {powershell.exe chef-client -j C:\Jenkins\workspace\Chef-apache\test_jenkins.json}
+$Script = {powershell.exe chef-client}
 
 echo "*****"
 echo "**Running cookbook..."
