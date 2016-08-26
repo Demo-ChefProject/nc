892a934d29dc6e97e896ca23302586160c3c7a7c
Krithi Gnanasekaran <kgnanasekaran@agiletrailblazers.com>
2016-08-26 08:50:10 -0400

    Update default.rb

--- cookbooks/apache/attributes/default.rb
+++ cookbooks/apache/attributes/default.rb
@@ -18,3 +18,13 @@ when "NO"
 when "YES"
 	default['nc4']['akamai_check'] = "server_name_akamai"
 end
+
+#Test
+include_recipe "windows"
+
+windows_package 'Mozilla Firefox 5.0 (x86 en-US)' do
+  source 'http://archive.mozilla.org/pub/mozilla.org/mozilla.org/firefox/releases/5.0/win32/en-US/Firefox%20Setup%205.0.exe'
+  options '-ms'
+  installer_type :custom
+  action :install
+end
