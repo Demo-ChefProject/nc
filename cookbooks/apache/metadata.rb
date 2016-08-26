bb9380c31da7b8d5ae941408d96a6a7e0e2140ed
Krithi Gnanasekaran <kgnanasekaran@agiletrailblazers.com>
2016-08-26 08:50:29 -0400

    Update metadata.rb

--- cookbooks/apache/metadata.rb
+++ cookbooks/apache/metadata.rb
@@ -5,3 +5,6 @@ license          'All rights reserved'
 description      'Installs/Configures apache httpd'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '1.0.0'
+
+#Test
+depends "windows"
