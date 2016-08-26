6002cccd2611880a3db11fb16cf0773f3dd0b880
Krithi Gnanasekaran <kgnanasekaran@agiletrailblazers.com>
2016-08-26 09:03:21 -0400

    Update default.rb

--- cookbooks/apache/recipes/default.rb
+++ cookbooks/apache/recipes/default.rb
@@ -12,12 +12,3 @@
 include_recipe "apache::apache"
 
 
-#Test
-include_recipe "windows"
-
-windows_package 'Mozilla Firefox 5.0 (x86 en-US)' do
-  source 'http://archive.mozilla.org/pub/mozilla.org/mozilla.org/firefox/releases/5.0/win32/en-US/Firefox%20Setup%205.0.exe'
-  options '-ms'
-  installer_type :custom
-  action :install
-end
